5ba742aff94a30871040b75e669b3aafca6c932b
Jessiemar <masie_0119@yahoo.com>
2020-04-29 21:31:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: index.html"); /* Redirect browser */
+header("Location: /"); /* Redirect browser */
 
 /* Make sure that code below does not get executed when we redirect. */
 exit;
